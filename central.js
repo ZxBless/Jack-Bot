ff36b39051cfc37d660f5b97356dda490ad3bee5
Bless <134533024+ZxBless@users.noreply.github.com>
2024-07-27 20:57:58 -0500

    Actualizar central.js

--- central.js
+++ central.js
@@ -521,7 +521,7 @@ Genera un crash en el dispositivo Android afectado, mostrando un mensaje de erro
           contextInfo: {
             externalAdReply: {
               showAdAttribution: true,
-              title: `¿Quieres ser usuario Premium?`,
+              title: `¿Quieres obtener acceso a funciones exclusivas?`,
               body: `Lee la siguiente información ;)`,
               thumbnailUrl: 'https://i.pinimg.com/736x/6e/e4/90/6ee490d70942a9e64746438c75f185d4.jpg', //https://i.ibb.co/80F70Nm/fonfovip.webp
               sourceUrl: global.link,
